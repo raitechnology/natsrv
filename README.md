27b0a145c710c100ef1192ea1657779a7297daf4
Chris Anderson <chris@raitechnology.com>
2021-02-15 22:57:00 -0800

    Add multiple clients for each rv mcast, add readme

--- README.md
+++ README.md
@@ -1,3 +1,170 @@
 # Rai NATS RV Bridge
 
-Description TODO
+1. [Description of the NATS RV Bridge](#description-of-the-nats-rv-bridge)
+2. [A NATS RV Bridge use case](#a-nats-rv-bridge-use-case)
+3. [The NATS RV Bridge limitations and issues](#the-nats-rv-bridge-limitations-and-issues)
+4. [Installing the NATS RV Bridge](#installing-the-nats-rv-bridge)
+
+## Description of the NATS RV Bridge
+
+Natsrv is a server that bridges RV over [NATS](https://nats.io) networking.  It
+does this by listening to RV clients on a host and transforming the
+publish/subscribe protocol into the NATS publish/subscribe protocol.  This
+allows legacy RV software to work in an environment without multicast
+networking, such as a cloud service.  The
+[nats-server clustering](https://docs.nats.io/nats-server/configuration/clustering)
+is designed to route subject addressed messages throughout a network by
+interconnecting nodes and maintaining the subscription interest of the
+cluster.  The subject address design and the wildcard support are compatible
+with RV design.
+
+## A NATS RV Bridge use case
+
+A RV network does not need much configuration, the primary resource usage that
+an organization must allocate to RV are the multicast addresses that RV uses
+to communicate from node to node.  This model of multicast addresses that
+underlies a network is maintained with the NATS RV Bridge.  The multicast
+address and the service is used as the user authentication method within
+NATS [multi tenancy](https://docs.nats.io/nats-server/configuration/securing_nats/accounts)
+accounts.
+
+This segregation of NATS RV Bridge services can be important, since the NATS RV
+Bridge routes messages in a binary format that other NATS clients will not
+understand without special codecs (this is the
+[codec](https://github.com/raitechnology/raimd/) used).
+
+There are [example](config) configurations for a 2 node cluster.  This diagram
+shows how the nodes would communicate over the NATS network.
+
+![natsrv](natsrv.svg)
+
+The multicast networks used are 225.5.5.5 and 226.6.6.6 over service 7000.  The
+accounts in the example configuration have this defined:
+
+```console
+accounts {
+  A: {
+    users: [
+      { user: "225.5.5.5:7000" }
+    ]
+  }
+  B: {
+    users: [
+      { user: "226.6.6.6:7000" }
+    ]
+  }
+}
+```
+
+The RV clients that use this network would use this command to connect to the
+natsrv_server:
+
+```console
+# On host 192.168.25.22
+$ sass_rv_client -daemon 7500 -network '192.168.25.0;225.5.5.5;226.6.6.6' -service 7000 -subject RSF.REC.INTC.O
+```
+
+The RV server that uses this network would use this command to connect to the
+natsrv_server:
+
+```console
+# On host 192.168.25.21
+$ sass_rv_server -daemon 7500 -network '192.168.25.0;225.6.6.6;226.5.5.5' -service 7000 -subject RSF.REC.INTC.O
+```
+
+This use case separates the send and recv networks, which is common in the RV
+world.  It is designed to separate the publishers from the subscribers,
+where the server is routing messages from a few high rate publishers
+through to a larger network of low rate subscribers.  This allows the
+publishers to scale, since each can have it's own logical network independent
+of the underlying physical network.  Multicast forces all nodes that are
+subscribed to see all of the traffic published.  By segregating the multicast
+each publisher does not interfere with the other publishers or the subscribers.
+For example, a publisher might use a network like this:
+
+```console
+$ sass_rv_publisher -daemon 7500 -network '192.168.25.0;225.5.5.5;224.4.4.4' -service 7000
+```
+
+And the router might use a network like this:
+
+```console
+$ sass_rv_router -daemon 7500 -network '192.168.25.0;226.6.6.6,224.4.4.4;225.5.5.5' -service 7000
+```
+
+The router now has 2 recv networks (226.6.6.6,224.4.4.4) for handling an
+additional publisher and the subscribers on separate logical networks.
+
+A simpler configuration for a NATS RV Brisge would be to not use a network
+parameter at all.  In this case the accounts configuration simply uses the
+service parameter:
+
+```console
+accounts {
+  A: {
+    users: [
+      { user: "7000" }
+    ]
+  }
+}
+```
+
+The RV commands would simply use this:
+
+```console
+$ sass_rv_client -daemon 7500 -service 7000 -subject RSF.REC.INTC.O
+```
+
+The NATS RV Bridge does not preconfigure the parameters used by the clients,
+since they are supplied on the command line of the client, and the NATS Server
+does not need to be configured with accounts.  The traffic in that case would
+route through the network without any traffic segregation.
+
+## The NATS RV Bridge limitations and issues
+
+These are issues that the NATS RV Bridge has in order of importance:
+
+1. The NATS Server will disconnect if the consumer falls to far behind,
+configured in seconds.  This needs to be transformed into a
+_RV.ERROR.SYSTEM.DATALOSS... message if messages are lost and the Bridge
+needs to reconnect after a timeout.
+
+2.  Squash multiple overlapping subscriptions.  If '>' and TEST are subscribed
+then the NATS server will publish messages for each subscripton using the
+[SID](https://docs.nats.io/nats-protocol/nats-protocol#sub) used for the
+subsciption.  The NATS RV Bridge does not deduplicate these messages and
+forwards them.  This causes the subscribers of '>' and TEST to see two messages
+published when a TEST message is published.  The workaround for this is
+to use another Bridge daemon to separate the subscriptions.  This isn't common
+but is useful for debugging to see all subjects on a wildcard through a node.
+
+3. The NATS Bridge only allows one service per process to be attached.  The
+workaround for this is to use multiple daemons for each service defined.  This
+may remain, since it is a good idea to separate the processes anyway.
+
+## Installing the NATS RV Bridge
+
+Binary / RPM installation will come with a release (not ready yet).  Compiling
+can be done by cloning with the submodules and making, using CentOS 7 or later.
+Ubuntu and Debian had also been known to work but the primary dev work is done
+under CentOS 8.
+
+```console
+$ git clone https://github.com/raitechnology/natsrv
+$ cd natsrv
+$ git submodule update --init --recursive
+# need one of: dnf_depend, yum_depend, apt_depend
+$ make dnf_depend
+sudo dnf -y install make gcc-c++ git redhat-lsb openssl-devel pcre2-devel chrpath
+Last metadata expiration check: 0:47:09 ago on Mon 15 Feb 2021 08:53:17 PM PST.
+Package make-1:4.2.1-10.el8.x86_64 is already installed.
+Package gcc-c++-8.3.1-5.1.el8.x86_64 is already installed.
+Package git-2.27.0-1.el8.x86_64 is already installed.
+Package redhat-lsb-4.1-47.el8.x86_64 is already installed.
+Package openssl-devel-1:1.1.1g-12.el8_3.x86_64 is already installed.
+Package pcre2-devel-10.32-2.el8.x86_64 is already installed.
+Package chrpath-0.16-7.el8.x86_64 is already installed.
+$ make
+# lots of output, the final binary will be in a build directory
+$ RH8_x86_64/bin/natsrv_server -help
+```
--- src/server.cpp
+++ src/server.cpp
@@ -16,18 +16,35 @@ using namespace sassrv;
 using namespace kv;
 
 struct Args : public MainLoopVars { /* argv[] parsed args */
-  int rv_port;
+  int          rv_port,
+               nats_port;
   const char * user,
              * passwd,
              * auth_token;
-  Args() : rv_port( 0 ), user( 0 ), passwd( 0 ), auth_token( 0 ) {}
+  Args() : rv_port( 0 ), nats_port( 0 ),
+           user( 0 ), passwd( 0 ), auth_token( 0 ) {}
 };
 
-struct MyListener : public EvRvListen, public EvNatsClient {
+struct MyListener : public EvRvListen, public EvNatsClient,
+                    public EvNatsClientNotify {
   void * operator new( size_t, void *ptr ) { return ptr; }
-  MyListener( kv::EvPoll &p ) : EvRvListen( p ), EvNatsClient( p ) {}
-  char svc_buf[ 16 ];
+  int             nats_port;
+  char            svc_buf[ 32 ];
+  EvNatsClient ** clients;
+  char         ** users;
+  size_t          client_cnt,
+                  connect_cnt,
+                  start_cnt,
+                  stop_cnt,
+                  network_cnt;
+  uint64_t        total_bytes_lost;
 
+  MyListener( kv::EvPoll &p ) : EvRvListen( p ), EvNatsClient( p ),
+                                nats_port( 0 ), clients( 0 ), users( 0 ),
+                                client_cnt( 0 ), connect_cnt( 0 ),
+                                start_cnt( 0 ), stop_cnt( 0 ), network_cnt( 0 ),
+                                total_bytes_lost( 0 ) {}
+  /* EvRvListen */
   virtual int start_host( void ) noexcept final {
     /*uint8_t * rcv = (uint8_t *) (void *) &this->mcast.recv_ip[ 0 ],
             * snd = (uint8_t *) (void *) &this->mcast.send_ip,
@@ -36,37 +53,140 @@ struct MyListener : public EvRvListen, public EvNatsClient {
     if ( len > sizeof( this->svc_buf ) - 1 ) {
       fprintf( stderr, "service too long\n" );
       this->EvNatsClient::name = NULL;
+      if ( this->EvNatsClient::user == this->svc_buf )
+        this->EvNatsClient::user = NULL;
     }
     else {
       ::memcpy( this->svc_buf, this->service, this->service_len );
       this->svc_buf[ this->service_len ] = '\0';
       this->EvNatsClient::name = this->svc_buf;
+      if ( this->EvNatsClient::user == NULL )
+        this->EvNatsClient::user = this->svc_buf;
     }
-    printf( "start_network:        service %.*s, \"%.*s\"\n",
-            (int) this->service_len, this->service, (int) this->network_len,
+    printf( "start_network:        service %.*s",
+            (int) this->service_len, this->service );
+    if ( this->network_len > 0 ) {
+      printf( ", \"%.*s\"", (int) this->network_len,
             this->network );
-    if ( ! this->EvNatsClient::connect( "127.0.0.1", 4222 ) ) {
-      printf( "failed\n" );
-      return -1;
+    }
+    printf( "\n" );
+
+    uint32_t mcast_ip[ RvMcast::MAX_RECV_MCAST + 1 ];
+    uint8_t  overlay[ RvMcast::MAX_RECV_MCAST + 1 ];
+    size_t   i, ip_cnt = 0;
+
+    this->connect_cnt      = 0;
+    this->start_cnt        = 0;
+    this->stop_cnt         = 0;
+    this->network_cnt      = 0;
+    this->total_bytes_lost = 0;
+
+    if ( this->mcast.recv_cnt > 0 ) {
+      bool send_overlaps_recv = false;
+      for ( i = 0; i < this->mcast.recv_cnt; i++ ) {
+        mcast_ip[ ip_cnt ] = this->mcast.recv_ip[ i ];
+        overlay[ ip_cnt ] = 1;
+        if ( mcast_ip[ ip_cnt ] == this->mcast.send_ip ) {
+          overlay[ ip_cnt ] |= 2;
+          send_overlaps_recv = true;
+        }
+        ip_cnt++;
+      }
+      if ( ! send_overlaps_recv ) {
+        mcast_ip[ ip_cnt ] = this->mcast.send_ip;
+        overlay[ ip_cnt ] = 2;
+        ip_cnt++;
+      }
+    }
+    if ( ip_cnt != 0 ) {
+      if ( ip_cnt > this->client_cnt ) {
+        this->clients = (EvNatsClient **) ::realloc( this->clients,
+                                   ip_cnt * sizeof( this->clients[ 0 ] ) );
+        this->users = (char **) ::realloc( this->users,
+                                   ip_cnt * sizeof( this->users[ 0 ] ) );
+
+        for ( ; this->client_cnt < ip_cnt; this->client_cnt++ ) {
+          this->clients[ this->client_cnt ] =
+            create_nats_client( this->EvRvListen::poll );
+          this->users[ this->client_cnt ]   = NULL;
+        }
+      }
+      for ( i = 0; i < ip_cnt; i++ ) {
+        char buf[ 32 ];
+        uint8_t * q;
+        int len;
+        q = (uint8_t *) &mcast_ip[ i ];
+        len = ::snprintf( buf, sizeof( buf ), "%u.%u.%u.%u:%.*s",
+                          q[ 0 ], q[ 1 ], q[ 2 ], q[ 3 ],
+                          (int) this->service_len, this->service );
+        this->users[ i ] = (char *) ::realloc( this->users[ i ], len + 1 );
+        ::strcpy( this->users[ i ], buf );
+        this->clients[ i ]->user = this->users[ i ];
+        if ( ( overlay[ i ] & 2 ) != 0 )
+          this->clients[ i ]->fwd_all_msgs = true;
+        else
+          this->clients[ i ]->fwd_all_msgs = false;
+        if ( ( overlay[ i ] & 1 ) != 0 )
+          this->clients[ i ]->fwd_all_subs = true;
+        else
+          this->clients[ i ]->fwd_all_subs = false;
+      }
+      for ( i = 0; i < ip_cnt; i++ ) {
+        if ( ! this->clients[ i ]->connect( "127.0.0.1", this->nats_port,
+                                            this ) )
+          break;
+        this->connect_cnt++;
+        this->network_cnt++;
+      }
+      if ( i != ip_cnt ) {
+        for ( size_t j = 0; j < i; j++ )
+          this->clients[ i ]->idle_push( EV_SHUTDOWN );
+        return -1;
+      }
+    }
+    else {
+      /*printf( "host_parameters:      " );
+      this->mcast.print();*/
+      if ( ! this->EvNatsClient::connect( "127.0.0.1", this->nats_port,
+                                          this ) ) {
+        printf( "failed\n" );
+        return -1;
+      }
+      this->connect_cnt++;
     }
     return 0;
-  }
-  virtual void on_connect( void ) noexcept final {
-    printf( "connected\n" );
-    this->EvRvListen::start_host();
-  }
-  virtual void on_shutdown( uint64_t bytes_lost ) noexcept final {
-    if ( bytes_lost != 0 )
-      printf( "bytes_lost %lu\n", bytes_lost );
-    printf( "shutdown\n" );
   }
   virtual int stop_host( void ) noexcept final {
-    printf( "stop_network:         service %.*s, \"%.*s\"\n",
-            (int) this->service_len, this->service, (int) this->network_len,
+    printf( "stop_network:         service %.*s",
+            (int) this->service_len, this->service );
+    if ( this->network_len > 0 ) {
+      printf( ", \"%.*s\"", (int) this->network_len,
             this->network );
+    }
+    printf( "\n" );
     this->EvRvListen::stop_host();
-    this->EvNatsClient::do_shutdown();
+    if ( this->network_cnt == 0 )
+      this->EvNatsClient::do_shutdown();
+    else {
+      for ( size_t i = 0; i < this->network_cnt; i++ )
+        this->clients[ i ]->do_shutdown();
+    }
     return 0;
+  }
+  /* EvNatsClientNotify */
+  virtual void on_connect( void ) noexcept final {
+    if ( ++this->start_cnt == this->connect_cnt ) {
+      printf( "connected\n" );
+      this->EvRvListen::start_host();
+    }
+  }
+  virtual void on_shutdown( uint64_t bytes_lost ) noexcept final {
+    this->total_bytes_lost += bytes_lost;
+    if ( ++this->stop_cnt == this->connect_cnt ) {
+      if ( this->total_bytes_lost != 0 )
+        printf( "bytes_lost %lu\n", this->total_bytes_lost );
+      printf( "shutdown\n" );
+    }
   }
 };
 
@@ -85,11 +205,13 @@ struct Loop : public MainLoop<Args> {
     this->rv_sv->user       = this->r.user;
     this->rv_sv->pass       = this->r.passwd;
     this->rv_sv->auth_token = this->r.auth_token;
+    this->rv_sv->nats_port  = this->r.nats_port;
     return true;
   }
   bool init( void ) {
     printf( "natsrv_version:       " kv_stringify( NATSRV_VER ) "\n" );
     printf( "rv_daemon:            %d\n", this->r.rv_port );
+    printf( "nats_daemon:          %d\n", this->r.nats_port );
     return this->rv_init();
   }
   static bool initialize( void *me ) noexcept {
@@ -113,6 +235,7 @@ main( int argc, const char *argv[] )
   r.no_default   = true;
   r.all          = true;
   r.add_desc( "  -p rv    = listen rv port        (7500)" );
+  r.add_desc( "  -n nats  = nats port             (4222)" );
   r.add_desc( "  -u user  = NATS user             (" NATS_USER_ENV ")" );
   r.add_desc( "  -x pass  = NATS password         (" NATS_PASS_ENV ")" );
   r.add_desc( "  -a auth  = NATS auth_token       (" NATS_TOKEN_ENV ")" );
@@ -122,6 +245,7 @@ main( int argc, const char *argv[] )
     return 1;
   shm.print();
   r.rv_port    = r.parse_port( argc, argv, "-p", "7500" );
+  r.nats_port  = r.parse_port( argc, argv, "-n", "4222" );
   r.user       = r.get_arg( argc, argv, 1, "-u", NULL, NATS_USER_ENV );
   r.passwd     = r.get_arg( argc, argv, 1, "-x", NULL, NATS_PASS_ENV );
   r.auth_token = r.get_arg( argc, argv, 1, "-a", NULL, NATS_TOKEN_ENV );
